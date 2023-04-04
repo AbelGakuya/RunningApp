68e0adb619c69a9fb55f8ce92c30ec9b88b3ccb8
AMG <abelgakuya@gmail.com>
2023-04-04 21:18:33 +0300

    Added an adapter package

---
+++ app/src/main/java/com/example/runningapp/adapters/RunAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.runningapp.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.runningapp.db.Run
+
+class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
+
+
+    inner class RunViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this,diffCallback)
+
+    fun submitList(list: List<Run>) = differ.submitList(list)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
