93e7cf056df555e6bbaca8de353270fbde68dc8e
AMG <abelgakuya@gmail.com>
2023-04-05 17:24:48 +0300

    Added a sorting program to try and sort the runs into different categories

--- app/src/main/java/com/example/runningapp/adapters/RunAdapter.kt
+++ app/src/main/java/com/example/runningapp/adapters/RunAdapter.kt
@@ -1,11 +1,21 @@
 package com.example.runningapp.adapters
 
+import android.icu.util.Calendar
+import android.os.Build
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.runningapp.R
 import com.example.runningapp.db.Run
+import com.example.runningapp.other.TrackingUtility
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
 
@@ -29,14 +39,46 @@ class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
     fun submitList(list: List<Run>) = differ.submitList(list)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
-        TODO("Not yet implemented")
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_run,
+                parent,
+                false
+            )
+        )
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val run = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(run.img).into(findViewById(R.id.ivRunImage))
+
+            val calendar = Calendar.getInstance().apply {
+                timeInMillis = run.timestamp
+            }
+
+            val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+            val tvDate = findViewById<TextView>(R.id.tvDate)
+            tvDate.text = dateFormat.format(calendar.time)
+
+            val avgSpeed = "${run.avgSpeedInKMH}km/h"
+            findViewById<TextView>(R.id.tvAvgSpeed).text = avgSpeed
+
+            val distanceInKm = "${run.distanceInMeters / 1000f}km"
+            findViewById<TextView>(R.id.tvDistance).text = distanceInKm
+
+            findViewById<TextView>(R.id.tvTime).text = TrackingUtility.getFormattedStopWatchTime(run.timeInMillis)
+
+            val caloriesBurned = "${run.caloriesBurned}kcal"
+            findViewById<TextView>(R.id.tvCalories).text = caloriesBurned
+
+
+
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return differ.currentList.size
     }
 }
