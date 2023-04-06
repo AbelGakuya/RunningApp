f2ddc7f67ea04450bb50a024aa5212b62327f4b8
AMG <abelgakuya@gmail.com>
2023-04-06 18:23:37 +0300

    Saving username and weight into shared preferences and later injecting their values into relevant fragments

--- app/src/main/java/com/example/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/SetupFragment.kt
@@ -1,16 +1,33 @@
 package com.example.runningapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentSetupBinding
+import com.example.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.runningapp.other.Constants.KEY_NAME
+import com.example.runningapp.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SetupFragment:Fragment() {
     private lateinit var binding: FragmentSetupBinding
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    @set:Inject
+    var isFirstAppOpen = true
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,11 +36,48 @@ class SetupFragment:Fragment() {
         binding = FragmentSetupBinding.inflate(inflater,container,false)
         val view = binding.root
 
+        if (!isFirstAppOpen){
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.setupFragment,true)
+                .build()
+            findNavController().navigate(
+                R.id.action_setupFragment_to_runFragment,
+                savedInstanceState,
+                navOptions
+            )
+        }
+
         val tvContinue = binding.tvContinue
         tvContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            val success = writePersonalDataToSharedPref()
+            if (success){
+                findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            } else {
+                Snackbar.make(requireView(), "Please Enter All the Fields!", Snackbar.LENGTH_SHORT).show()
+            }
+
+
         }
 
         return view
     }
+
+    private fun writePersonalDataToSharedPref() : Boolean{
+        val name = binding.etName.text.toString()
+        val weight = binding.etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+
+        sharedPref.edit()
+            .putString(KEY_NAME,name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
+            .apply()
+
+        val toolbarText = "Let's go, $name!"
+        val toolbar = requireActivity().findViewById<Toolbar>(R.id.tvToolbarTitle)
+        return true
+    }
+
 }
