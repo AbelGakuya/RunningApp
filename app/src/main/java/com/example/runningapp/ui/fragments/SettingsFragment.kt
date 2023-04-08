9f07be72fe9eee9d7048444c087912a9c8c83eb1
AMG <abelgakuya@gmail.com>
2023-04-08 11:23:16 +0300

    Changed name and weight parameters in shared preferences

--- app/src/main/java/com/example/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,72 @@
 package com.example.runningapp.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.runningapp.R
+import com.example.runningapp.databinding.FragmentSettingsBinding
+import com.example.runningapp.other.Constants.KEY_NAME
+import com.example.runningapp.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment:Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+
+        binding.btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if (success){
+                Snackbar.make(view,"Saved changes", Snackbar.LENGTH_LONG).show()
+            } else{
+                Snackbar.make(view,"Please fill out all fields", Snackbar.LENGTH_LONG).show()
+            }
+
+
+        }
+        return view
+    }
+
+    private fun loadFieldsFromSharedPref(){
+        val name = sharedPreferences.getString(KEY_NAME,"")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT,80f)
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean{
+
+        val name = binding.etName.text.toString()
+        val weight = binding.etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT,weight.toFloat())
+            .apply()
+
+        val toolText = "Let's go, $name!"
+        requireActivity().findViewById<androidx.appcompat.widget.Toolbar>(R.id.tvToolbarTitle).title = toolText
+        return true
+
+    }
 }
